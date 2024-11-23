9988631f92f7c639ad7a20422a2680641bc22efd
nikhil-dev-395 <nikhilw395@gmail.com>
2024-11-23 19:06:00 +0530

    share handled v3

--- public/scripts/shareLink.scripts.js
+++ public/scripts/shareLink.scripts.js
@@ -6,7 +6,7 @@ let content = "this is sharable content";
 const shareBtn = document.querySelectorAll("#shareBtn");
 
 shareBtn.forEach((button, index) => {
-  const fileDownloadUrl = button.dataset.downloadUrl;
+  const fileDownloadUrl = button.dataset.sharefileUrl;
   button.addEventListener("click", () => {
     let whatsAppUrl = `https://wa.me/?text=${encodeURIComponent(
       fileDownloadUrl
